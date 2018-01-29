b0c8a4862242bc0b188dcfe3f8dbeef5323b9686
dhirajranjit <ranjitd11@mycu.concord.edu>
2018-01-29 00:23:39 -0500

    Change

---
+++ src/automationFramework/FirstTest.java
@@ -0,0 +1,20 @@
+package automationFramework;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstTest {
+
+	public static void main(String[] args) throws InterruptedException {
+		// Create a new instance of the Firefox driver
+							
+	
+        System.out.println("Passed");
+
+       
+     
+	}
+
+}
